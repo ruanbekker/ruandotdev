18e62da7b929138b541088fe7a617237304f644d
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-06-29 20:53:53 +0200

    lotsofchanges-1561834433

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -ex
-export DOMAIN=MY_APP_DOMAIN
-#source /tmp/ruan.dev/env.sh
+#export DOMAIN=MY_APP_DOMAIN
+source /tmp/ruan.dev/environment.sh
 env
 docker stack deploy -c /tmp/ruan.dev/docker-compose.yml blogs
